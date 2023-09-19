497ced788ecc6a566d8f7d9f5fc0fa42472ba72b
aaronmullan <aaroncmullan@gmail.com>
2023-09-19 09:58:17 -0700

    config change

--- next.config.js
+++ next.config.js
@@ -4,6 +4,13 @@ const nextConfig = {
   images: {
     domains: ["avatars.githubusercontent.com", "github.com"],
   },
+  experimental: {
+    // This is experimental but can
+    // be enabled to allow parallel threads
+    // with nextjs automatic static generation
+    workerThreads: false,
+    cpus: 1,
+  },
 };
 
 module.exports = nextConfig;
